3997661a188a0402429be7b1406dcec23eea89c6
ricardoBpinheiro <ricardo.pinheiro@catolicasc.org.br>
2020-05-29 00:06:56 +0000

    Conecta ao servidor

--- client.c
+++ client.c
@@ -1,9 +1,14 @@
+#include <netinet/in.h>
 #include <stdio.h>
 #include <sys/socket.h>
+#include <arpa/inet.h> // inet_addr
 
 int main(int argc, char *argv[]){
     
-    int socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    int socket_desc;
+    struct sockaddr_in server;
+
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
     // AF_INET = IPV4
     // SOCK_STREAM = TCP
     // 0 = PROTOCOLO IP
@@ -12,6 +17,20 @@ int main(int argc, char *argv[]){
         printf("Não foi possivel criar o socket");
         return 1;
     }
+
+    //Informa os dados do servidor para onde vamos nos conectar
+    server.sin_addr.s_addr = inet_addr("172.217.28.132");  // Converte IP para long
+    server.sin_family = AF_INET; //informa que é um IPV4
+    server.sin_port = htons(80);  //Especifica a porta
+
+
+    //Conecta ao servidor 
+    if (connect(socket_desc, (struct sockaddr *) &server, sizeof(server)) < 0){
+        printf("Erro ao conectar-se\n");
+        return 1;
+    }
+
+    printf("Conectado.\n");
 
     return 0;
 }
